81b74cd4d738b1ea13c2aad424a9a407b09e21a2
Rami Bokhari <rbisthebest@hotmail.com>
2019-12-10 12:17:14 +0300

    Updates

---
+++ precache-manifest.1a2faf7bf49336d4dc8432a7a35237b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa20ad6176b92e560f8e5c509772909f",
+    "url": "/RamiB1234/portfolio/index.html"
+  },
+  {
+    "revision": "d37efa48521253cd3a65",
+    "url": "/RamiB1234/portfolio/static/css/2.b085df62.chunk.css"
+  },
+  {
+    "revision": "f9b8a58ef694db1d9c44",
+    "url": "/RamiB1234/portfolio/static/css/main.da41bb43.chunk.css"
+  },
+  {
+    "revision": "d37efa48521253cd3a65",
+    "url": "/RamiB1234/portfolio/static/js/2.0a66f0cd.chunk.js"
+  },
+  {
+    "revision": "f9b8a58ef694db1d9c44",
+    "url": "/RamiB1234/portfolio/static/js/main.e99e1a5b.chunk.js"
+  },
+  {
+    "revision": "bee8b2a49c8be881cd6d",
+    "url": "/RamiB1234/portfolio/static/js/runtime~main.d4f407e7.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/RamiB1234/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/RamiB1234/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/RamiB1234/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/RamiB1234/portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
