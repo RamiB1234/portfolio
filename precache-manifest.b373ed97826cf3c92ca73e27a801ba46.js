9d00f4d56b3ee42d9443307fd216cf37e3204313
RamiB1234 <rbisthebest@hotmail.com>
2021-06-09 13:17:57 +0300

    Updates

---
+++ precache-manifest.b373ed97826cf3c92ca73e27a801ba46.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ac7e4569a619fc7d4c394ac229b055",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ecc7379a321d1b8a91e8",
+    "url": "/portfolio/static/css/2.f7c64eb8.chunk.css"
+  },
+  {
+    "revision": "a7fb2ee5c791ac0f7866",
+    "url": "/portfolio/static/css/main.f5637df9.chunk.css"
+  },
+  {
+    "revision": "ecc7379a321d1b8a91e8",
+    "url": "/portfolio/static/js/2.78727e7f.chunk.js"
+  },
+  {
+    "revision": "a7fb2ee5c791ac0f7866",
+    "url": "/portfolio/static/js/main.9a0cf8e8.chunk.js"
+  },
+  {
+    "revision": "aa06d906e2d75e22cb9d",
+    "url": "/portfolio/static/js/runtime~main.5bba913d.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
